92d355711ea7eae9dcf90b94ba114c0b7f01aa36
Eynav Ptia <onevptaya@gmail.com>
2022-03-02 13:22:34 +0200

    setting binding parameters

--- app/src/main/java/com/example/changetheworld/MainActivity.java
+++ app/src/main/java/com/example/changetheworld/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.changetheworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.changetheworld.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     Button createAccount;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         createAccount = (Button) findViewById(R.id.createAccount);
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openCreateNewAccount();
             }
+        });
+        binding.login.setOnClickListener(view->{
+            String userName = binding.editTextTextPersonName.getText().toString();
+            String password = binding.editTextTextPassword.getText().toString();
+            Log.d("Main","button pressed" + userName + " " + password);
         });
     }
 
