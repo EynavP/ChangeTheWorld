df4bb3e5a57c254c65e55f0f93f3b806ef5a4ec2
Eynav Ptia <onevptaya@gmail.com>
2022-03-11 18:32:59 +0200

    starting business home page

---
+++ app/src/main/java/com/example/changetheworld/BusinessHomePage.java
@@ -0,0 +1,13 @@
+package com.example.changetheworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusinessHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_home_page);
+    }
+}
