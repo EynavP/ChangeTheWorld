b86a8dc51a9642f88f14067f5459400d86a1b986
niv vaknin <39972445+vaknin34@users.noreply.github.com>
2022-04-13 14:52:17 +0300

    add location to app

---
+++ app/src/main/java/com/example/changetheworld/model/LocationDataApi.java
@@ -0,0 +1,45 @@
+package com.example.changetheworld.model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class LocationDataApi implements LocationDataApiInterface{
+
+    private String base_api = "http://193.106.55.105:80?location=";
+
+    @Override
+    public Float GetDistance(String address_1, String address_2) {
+        String query = base_api + address_1 + '/' + address_2;
+        URL url = null;
+        try {
+            url = new URL(query);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Scanner s = new Scanner(new BufferedReader(new InputStreamReader(in)));
+            String result = "";
+            while (s.hasNext()){
+                result += s.next();
+            }
+            urlConnection.disconnect();
+            JSONArray data = new JSONArray(result);
+            return Float.parseFloat((String) data.get(0));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/changetheworld/model/LocationDataApiInterface.java
@@ -0,0 +1,5 @@
+package com.example.changetheworld.model;
+
+public interface LocationDataApiInterface {
+    Float GetDistance(String address_1, String address_2);
+}
