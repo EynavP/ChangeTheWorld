ca5d1f4dd6dd717b77ef61ede1c24b9adbb880a3
niv vaknin <39972445+vaknin34@users.noreply.github.com>
2022-04-13 14:29:48 +0300

    ADD location to server

--- CurrencyServer/server.py
+++ CurrencyServer/server.py
@@ -2,15 +2,32 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
 import time
 import json
+from urllib import response
 from urllib.parse import parse_qs, urlparse
 import threading
 from datetime import datetime
 import requests
 import urllib.request
+from geopy.geocoders import Nominatim
+import haversine as hs
 
-#external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-hostName = "localhost"
-serverPort = 8080
+
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+hostName = "0.0.0.0"
+serverPort = 80
+
+
+
+class LocationApi:
+    def __init__(self) -> None:
+        self.loc = Nominatim(user_agent="GetLoc")
+
+    def CalculateDistance(self, address_1: str, address_2: str) -> float:
+        getLoc1 = self.loc.geocode(address_1)
+        getLoc2 = self.loc.geocode(address_2)
+        loc1 = (getLoc1.latitude,getLoc1.longitude)
+        loc2 = (getLoc2.latitude,getLoc2.longitude)
+        return hs.haversine(loc1,loc2)
 
 
 class CurrencyApi:
@@ -40,10 +57,10 @@ class CurrencyApi:
                 for item in data_list:
                     self.data[item['s']] = (item['c'], item['cp'])
                 self.lock.release()
-                time.sleep(900)
+                time.sleep(40000)
             except Exception as e:
                 print("Api Update error" + str(e))
-                time.sleep(900)
+                time.sleep(40000)
                 continue
         
     def run(self):
@@ -52,37 +69,54 @@ class CurrencyApi:
 
 class MyServer(BaseHTTPRequestHandler):
 
-    def set_API(self, api: CurrencyApi):
-        self.api = api
+    def set_API(self, api_currency: CurrencyApi, api_location: LocationApi):
+        self.api_currency = api_currency
+        self.api_location = api_location
 
 
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        quert_components = parse_qs(urlparse(self.path).query)
-        try:
-            symbols = quert_components['symbol'][0]
-            symbols = symbols.split(",")
-            response = []
-            for sym in symbols:
-                if sym not in self.api.data.keys():
-                    raise Exception("symbol : " + sym + "not found")
-                response.append({"s" : sym, "c": self.api.data[sym][0], "cp": self.api.data[sym][1]})
+        query_components = parse_qs(urlparse(self.path).query)
+        try:            
+            if 'symbol' in query_components.keys():
+                response = self.get_currency_data(query_components)
+            
+            if 'location' in query_components.keys():
+                response = self.get_location(query_components)
+
             self.wfile.write(bytes(json.dumps(response),encoding='utf8'))
         except Exception as e:
             self.wfile.write(bytes(json.dumps({"Error": str(e), "time": str(datetime.now())}),encoding='utf8'))
               
+    def get_currency_data(self, query_components) -> list:
+            symbols = query_components['symbol'][0]
+            symbols = symbols.split(",")
+            response = []
+            for sym in symbols:
+                if sym not in self.api_currency.data.keys():
+                    raise Exception("symbol : " + sym + "not found")
+                response.append({"s" : sym, "c": self.api_currency.data[sym][0], "cp": self.api_currency.data[sym][1]})
+            return response
+    
+    def get_location(self, query_components) -> list:
+        address = query_components['location'][0]
+        address_1 = address.split('/')[0]
+        address_2 = address.split('/')[1]
+        return [self.api_location.CalculateDistance(address_1=address_1, address_2=address_2)]
+
+
 
 
 if __name__ == "__main__":
-    api = CurrencyApi()
-    api.run()
+    api_currency = CurrencyApi()
+    api_location = LocationApi()
+    api_currency.run()
     my_server = MyServer
-    my_server.set_API(my_server,api=api)
+    my_server.set_API(my_server,api_currency=api_currency, api_location=api_location)
     webServer = HTTPServer((hostName, serverPort), my_server)
-    print(f"Server started http://{hostName}:{serverPort}")
-    #or http://{external_ip}:{serverPort} 
+    print(f"Server started http://{hostName}:{serverPort} or http://{external_ip}:{serverPort}") 
 
     try:
         webServer.serve_forever()
