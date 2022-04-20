fb9e7d2866e2ad2be7df779a4b80f8b4ace59e27
yuvalamir123 <yuval.amir998@gmail.com>
2022-04-20 13:33:12 +0300

    progress bar for search results

--- app/src/main/java/com/example/changetheworld/search_result_page.java
+++ app/src/main/java/com/example/changetheworld/search_result_page.java
@@ -5,22 +5,40 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.changetheworld.model.FireStoreDB;
 
 public class search_result_page extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result_page);
         recyclerView = findViewById(R.id.SearchRecycle);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        progressBar = findViewById(R.id.progressBar);
+
+        if(progressBar.getVisibility() != View.INVISIBLE) {
+            progressBar.setVisibility(View.VISIBLE);
+        }
         String state = getIntent().getStringExtra("state");
         String city = getIntent().getStringExtra("city");
         String street = getIntent().getStringExtra("street");
         String number = getIntent().getStringExtra("number");
         FireStoreDB.getInstance().searchChange(state, city, street, number, recyclerView, this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar = findViewById(R.id.progressBar);
+
+        if(progressBar.getVisibility() != View.INVISIBLE) {
+            progressBar.setVisibility(View.VISIBLE);
+        }
     }
 }
