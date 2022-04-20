2f6c5e65e481f29f351669ac2f1d1434b10f5356
yuvalamir123 <yuval.amir998@gmail.com>
2022-04-20 20:02:24 +0300

    fix business profil details

--- app/src/main/java/com/example/changetheworld/BusinessProfileActivity.java
+++ app/src/main/java/com/example/changetheworld/BusinessProfileActivity.java
@@ -5,8 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.changetheworld.model.FireStoreDB;
 
 public class BusinessProfileActivity extends AppCompatActivity {
+
+    String userName;
+    TextView business_name;
+    TextView mail_address;
+    TextView phone_number;
+    TextView owner_name;
+    TextView state;
+    TextView city;
+    TextView street;
+    TextView number;
 
     Button editBtn;
 
@@ -15,11 +28,24 @@ public class BusinessProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_profile);
 
+        business_name = findViewById(R.id.business_name_value);
+        mail_address = findViewById(R.id.maillAddress_value);
+        phone_number = findViewById(R.id.phoneNumber_value);
+        owner_name = findViewById(R.id.business_owner_name_value);
+        state = findViewById(R.id.state_value);
+        city = findViewById(R.id.city_value);
+        street = findViewById(R.id.street_value);
+        number = findViewById(R.id.number_value);
+
+        userName = getIntent().getStringExtra(getString(R.string.userName));
+        ((TextView)findViewById(R.id.business_username_profile_name)).setText(userName);
+        FireStoreDB.getInstance().loadBusinessData(userName, business_name, mail_address, phone_number, owner_name, state, city, street, number);
+
         editBtn = findViewById(R.id.editBtn);
-        editBtn.setOnClickListener(view -> {OpenEitProfileBusiness();});
+        editBtn.setOnClickListener(view -> {OpenEditProfileBusiness();});
     }
 
-    private void OpenEitProfileBusiness() {
+    private void OpenEditProfileBusiness() {
         Intent intent = new Intent(this,EditBusinessProfileActivity.class);
         startActivity(intent);
     }
